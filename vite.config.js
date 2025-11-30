8f3af04fc4f9f2df0a3e59a426e6e446471a0960
Proyect Manager <ggeo@gmail.com>
2025-12-01 00:30:25 +0100

    FIX: Configuración completa 2 para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-/**
- * Configuración de Vite para el proyecto Nayla
- * 
- * @type {import('vite').UserConfig}
- */
 export default defineConfig({
   plugins: [react()],
-  base: './', // ¡IMPORTANTE! Esto debe coincidir con el nombre del repositorio
-  server: {
-    port: 3000,
-    open: true // Abre el navegador automáticamente
-  },
+  base: '/nayla-website/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    assetsDir: 'assets'
   }
 })
